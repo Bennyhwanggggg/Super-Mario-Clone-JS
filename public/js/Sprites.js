0d37f762a8a603c9b6164a1c60faae05088e0af9
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 19:32:18 +1000

    add loading functions

---
+++ public/js/Sprites.js
@@ -0,0 +1,21 @@
+import SpriteSheet from './SpriteSheet.js';
+import { loadImage } from './loaders.js'
+
+export function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.defineTile('ground', 0, 0);
+        sprites.defineTile('sky', 3, 23);
+        return sprites;
+    });
+}
+
+export function loadMarioSprite() {
+    return loadImage('/img/characters.gif')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.define('idle', 276, 44, 16, 16);
+        return sprites;
+    });
+}

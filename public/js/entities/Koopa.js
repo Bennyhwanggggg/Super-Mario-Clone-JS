f7b052e4851475a5e751397da13abe99a07c653a
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 22:37:26 +1000

    fix imports

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,4 +1,5 @@
-import Entity, { Sides, Trait} from '../Entity.js';
+import Entity from '../Entity.js';
+import Trait from '../Trait.js';
 import Killable from '../traits/Killable.js';
 import PendulumMove from '../traits/PendulumMove.js';
 import Physics from '../traits/Physics.js';
--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -1,4 +1,4 @@
-import { Trait } from '../Trait.js';
+import Trait from '../Trait.js';
 
 export default class Go extends Trait {
     constructor() {

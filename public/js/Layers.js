8fff00f07d2dfb1c6ea40c079c23a52ef3410af6
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 20:52:02 +1000

    add draw collision layer

--- public/js/Layers.js
+++ public/js/Layers.js
@@ -1,13 +1,3 @@
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    })
-}
-
 export function createBackgroundLayer(backgrounds, sprites) {
     const buffer = document.createElement('canvas');
     buffer.width = 256; 
@@ -25,5 +15,41 @@ export function createBackgroundLayer(backgrounds, sprites) {
 export function createSpriteLayer(entity) {
     return function drawSpriteLayer(context) {
         entity.draw(context);
+    };
+}
+
+export function createCollisionLayer(level) {
+    const resolvedTiles = [];
+
+    const tileResolver = level.tileCollider.tiles;
+    const tileSize = tileResolver.tileSize;
+
+    const getByIndexOriginal = tileResolver.getByIndex;
+    tileResolver.getByIndex = function getByIndexFake(x, y) {
+        resolvedTiles.push({x, y});
+        return getByIndexOriginal.call(tileResolver, x, y);
+    }
+
+    return function drawCollision(context) {
+        context.strokeStyle = 'blue';
+        resolvedTiles.forEach(({x, y}) => {
+            context.beginPath();
+            context.rect(
+                x * tileSize,
+                y * tileSize,
+                tileSize, tileSize);
+            context.stroke();
+        });
+
+        context.strokeStyle = 'red';
+        level.entities.forEach(entity => {
+            context.beginPath();
+            context.rect(
+                entity.pos.x, entity.pos.y,
+                entity.size.x, entity.size.y);
+            context.stroke();
+        });
+
+        resolvedTiles.length = 0;
     };
 }

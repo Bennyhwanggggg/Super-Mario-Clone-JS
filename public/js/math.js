0c63f2a71e2aa004144bd542d68b2c3237d1d291
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 20:49:46 +1000

    add matrix

--- public/js/math.js
+++ public/js/math.js
@@ -9,3 +9,29 @@ export class Vec2 {
         this.y = y;
     }
 }
+
+export class Matrix {
+    constructor() {
+        this.grid = [];
+    }
+
+    forEach(callback) {
+        this.grid.forEach((column, x) => {
+            column.forEach((value, y) => {
+                callback(value, x, y);
+            })
+        })
+    }
+
+    get(x, y) {
+        const col = this.grid[x];
+        return col ? col[y] : undefined;
+    }
+
+    set(x, y, value) {
+        if (!this.grid[x]) {
+            this.grid[x] = [];
+        }
+        this.grid[x][y] = value;
+    }
+}

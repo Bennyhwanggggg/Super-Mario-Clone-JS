ef8de9542296848bfdfc2028d25c5907155c9e74
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 20:58:30 +1000

    refactor level loader

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,3 +1,7 @@
+import Level from './Level.js';
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
+import { loadBackgroundSprites } from './sprites.js';
+
 export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
@@ -8,7 +12,35 @@ export function loadImage(url) {
     });
 }
 
+function createTiles(level, backgrounds) {
+    backgrounds.forEach(background => {
+        background.ranges.forEach(([x1, x2, y1, y2]) => {
+            for (let x = x1; x < x2; ++x) {
+                for (let y = y1; y < y2; ++y) {
+                    level.tiles.set(x, y, {
+                        name: background.tile,
+                    });
+                }
+            }
+        });
+    });
+}
+
 export function loadLevel(name) {
-    return fetch(`/levels/${name}.json`)
-    .then(r => r.json());
+    return Promise.all([
+        fetch(`/levels/${name}.json`)
+        .then(r => r.json()),
+        loadBackgroundSprites(),
+    ])
+    .then(([levelSpec, backgroundSprites]) => {
+        const level = new Level();
+        createTiles(level, levelSpec.backgrounds);
+
+        const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
+        level.comp.layers.push(backgroundLayer);
+
+        const spriteLayer = createSpriteLayer(level.entities);
+        level.comp.layers.push(spriteLayer);
+        return level;
+    })
 }

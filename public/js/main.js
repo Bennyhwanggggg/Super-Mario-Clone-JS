b2b06b20b15329e50449ee65a7d46ad7f18b7bf3
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 19:09:12 +1000

    draw sky and ground and add draw tile function

--- public/js/main.js
+++ public/js/main.js
@@ -11,5 +11,16 @@ loadImage('/img/tiles.png')
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
-    sprites.draw('sky', context, 45, 62);
+
+    for (let x = 0; x < 25; ++x) {
+        for (let y = 0; y < 14; ++y) {
+            sprites.drawTile('sky', context, x, y);
+        }
+    }
+
+    for (let x = 0; x < 25; ++x) {
+        for (let y = 12; y < 14; ++y) {
+            sprites.drawTile('ground', context, x, y);
+        }
+    }
 })

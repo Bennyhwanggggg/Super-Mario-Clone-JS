6591c0ba6aa0bd9dbc7359607f15d424621a9dee
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 22:21:57 +1000

    update Trait

--- public/js/Trait.js
+++ public/js/Trait.js
@@ -1,9 +1,28 @@
 export default class Trait {
     constructor(name) {
         this.NAME = name;
+
+        this.tasks = [];
+    }
+
+    finalize() {
+        this.tasks.forEach(task => task());
+        this.tasks.length = 0;
+    }
+
+    queue(task) {
+        this.tasks.push(task);
+    }
+
+    collides(us, them) {
+
+    }
+
+    obstruct() {
+
     }
 
     update() {
-        console.log('123')
+
     }
 }

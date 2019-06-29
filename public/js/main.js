a8906909ce2990bd209b385c425977d6e6ccbb49
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 19:18:26 +1000

    setup background tiles loader

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 import SpriteSheet from './SpriteSheet.js';
-import { loadImage } from './loaders.js'
+import { loadImage, loadLevel } from './loaders.js'
 
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
+    })
+}
 
 
 const canvas = document.getElementById('screen');
@@ -12,15 +21,10 @@ loadImage('/img/tiles.png')
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
 
-    for (let x = 0; x < 25; ++x) {
-        for (let y = 0; y < 14; ++y) {
-            sprites.drawTile('sky', context, x, y);
-        }
-    }
-
-    for (let x = 0; x < 25; ++x) {
-        for (let y = 12; y < 14; ++y) {
-            sprites.drawTile('ground', context, x, y);
-        }
-    }
+    loadLevel('1-1')
+    .then(level => {
+        level.backgrounds.forEach(background => {
+            drawBackground(background, context, sprites)
+        })
+    })
 })

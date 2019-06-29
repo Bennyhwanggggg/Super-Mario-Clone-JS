430e732c573a8730fe0e15f124c3597095749e49
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 22:28:00 +1000

    clean up main

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,60 @@
 import Camera from './Camera.js';
+import Entity from './Entity.js';
+import PlayerController from './traits/PlayerController.js';
 import Timer from './Timer.js';
-import { loadLevel } from './loaders.js';
-import { createMario } from './entities.js';
-import { createCollisionLayer, createCameraLayer } from './layers.js';
+import { createLevelLoader } from './loaders/level.js';
+import { loadFont } from './loaders/font.js';
+import { loadEntities } from './entities.js';
 import { setupKeyboard } from './input.js';
-import { setupMouseControl } from './debug.js';
+import { createCollisionLayer } from './layers/collision.js';
+import { createDashboardLayer } from './layers/dashboard.js';
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
+function createPlayerEnv(playerEntity) {
+    const playerEnv = new Entity();
+    const playerControl = new PlayerController();
+    playerControl.checkpoint.set(64, 64);
+    playerControl.setPlayer(playerEntity);
+    playerEnv.addTrait(playerControl);
+    return playerEnv;
+}
 
-Promise.all([
-    createMario(),
-    loadLevel('1-1'),
-])
-.then(([mario, level]) => {
+async function main(canvas) {
+    const context = canvas.getContext('2d');
+
+    const [entityFactory, font] = await Promise.all([
+        loadEntities(),
+        loadFont(),
+    ]);
+
+    const loadLevel = await createLevelLoader(entityFactory);
+
+    const level = await loadLevel('1-1');
+
     const camera = new Camera();
-    window.camera = camera;
 
-    mario.pos.set(64, 64);
+    const mario = entityFactory.mario();
 
-    level.comp.layers.push(
-        createCollisionLayer(level),
-        createCameraLayer(camera));
+    const playerEnv = createPlayerEnv(mario);
+    level.entities.add(playerEnv);
 
 
-    level.entities.add(mario);
+    level.comp.layers.push(createCollisionLayer(level));
+    level.comp.layers.push(createDashboardLayer(font, playerEnv));
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
-
-    setupMouseControl(canvas, mario, camera);
-
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
         level.update(deltaTime);
 
+        camera.pos.x = Math.max(0, mario.pos.x - 100);
+
         level.comp.draw(context, camera);
     }
 
     timer.start();
-});
+}
+
+const canvas = document.getElementById('screen');
+main(canvas);

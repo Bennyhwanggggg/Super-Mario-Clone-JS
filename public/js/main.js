a7530ec2a0ae7ae07506924a9ca944d0f20a0b05
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 19:23:33 +1000

    refactor sprites and image loading

--- public/js/main.js
+++ public/js/main.js
@@ -11,20 +11,25 @@ function drawBackground(background, context, sprites) {
     })
 }
 
+function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.define('ground', 0, 0);
+        sprites.define('sky', 3, 23);
+        return sprites;
+    });
+}
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-loadImage('/img/tiles.png')
-.then(image => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 23);
-
-    loadLevel('1-1')
-    .then(level => {
-        level.backgrounds.forEach(background => {
-            drawBackground(background, context, sprites)
-        })
+Promise.all([
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([sprites, level]) => {
+    level.backgrounds.forEach(background => {
+        drawBackground(background, context, sprites)
     })
-})
+});

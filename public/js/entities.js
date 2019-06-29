4cd69be450adc47bbb75e785d7a343c08c3339ad
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-29 21:12:04 +1000

    add Go into traits for mario

--- public/js/entities.js
+++ public/js/entities.js
@@ -2,12 +2,14 @@ import Entity from './Entity.js';
 import Velocity from './traits/Velocity.js';
 import Jump from './traits/Jump.js';
 import { loadMarioSprite } from './sprites.js';
+import Go from './traits/Go.js';
 
 export function createMario() {
     return loadMarioSprite()
     .then(sprite => {
         const mario = new Entity();
 
+        mario.addTrait(new Go());
         mario.addTrait(new Velocity());
         mario.addTrait(new Jump());
 

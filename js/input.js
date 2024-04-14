24adeaa8933efd0171be730e89411ec0a25c9a22
Benny Hwang <32606136+Bennyhwanggggg@users.noreply.github.com>
2024-04-14 17:22:21 +1000

    Updates

---
+++ js/input.js
@@ -0,0 +1,27 @@
+import Keyboard from './KeyboardState.js';
+
+export function setupKeyboard(entity) {
+    const input = new Keyboard();
+
+    input.addMapping('Space', keyState => {
+        if (keyState) {
+            entity.jump.start();
+        } else {
+            entity.jump.cancel();
+        }
+    });
+
+    input.addMapping('KeyC', keyState => {
+        entity.turbo(keyState);
+    });
+
+    input.addMapping('ArrowRight', keyState => {
+        entity.go.dir += keyState ? 1 : -1;
+    });
+
+    input.addMapping('ArrowLeft', keyState => {
+        entity.go.dir += keyState ? -1 : 1;
+    });
+
+    return input;
+}
